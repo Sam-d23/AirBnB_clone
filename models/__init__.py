9015ceb60e5b4a1e54d848cf7a08cd0c3c857705
Sam-d23 <samkiragu05@gmail.com>
2024-01-20 01:09:50 -0800

    3.0

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+from models.engine.file_storage import FileStorage
+"""Creates variable storage, that class reload"""
+
+storage = FileStorage()
+storage.reload()
---
+++ models/base_model.py
@@ -0,0 +1,34 @@
+#!usr/bin/python3
+from uuid import uuid4
+from datetime import datetime
+import models
+"""Defines the class BaseModel"""
+class BaseModel:
+    """Initializes basemodel"""
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            self.__dict__.update(kwargs)
+            if created_at in kwargs:
+                self.created_at = datetime.fromisoformat(kwargs["created_at"])
+            if updated_at in kwargs:
+                self.updated_at = datetime.fromisoformat(kwargs["updated_at"])
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        models.storage.new(self)
+
+    def save(self):
+        """Updates the update_at with the curent time"""
+        models.storage.save()
+    def to_dict(self):
+        """returns the dict.self"""
+        model_dict = self.__dict__.copy()
+        model_dict["__class__"] = self.__class__.__name__
+        model_dict["created_at"] = self.created_at.isoformat()
+        model_dict["updated_at"] = self.updated_at.isoformat()
+        return model_dict
+    def __str__(self):
+        """Prints the [<class name>] (<self.id>) <self.__dict__>"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
